b8a9869391a9ca9a27de8fa590656de81fb5beee
monsaw <lawalmonsaw@gmail.com>
2022-07-25 09:11:49 +0100

    All completed....

--- src/main/java/com/example/fashionblog/FashionBlogApplication.java
+++ src/main/java/com/example/fashionblog/FashionBlogApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FashionBlogApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(FashionBlogApplication.class, args);
     }
 
